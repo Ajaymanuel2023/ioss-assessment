a0b87ac62ee9c3ff277404abea000804530e06b0
Ajaymanuel2023 <ajaymanuel2071@gmail.com>
2025-08-13 22:11:13 +0530

    Fix backend folder with FastAPI hello world

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,5 @@
 from fastapi import FastAPI
-
 app = FastAPI()
-
 @app.get("/")
 def read_root():
-    return {"message": "Hello from FastAPI"}
-
-
+    return {"message": "Hello World from Render!"}
